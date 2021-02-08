84a3f68fa22bc90bc01709bc87864927f9cd828c
cat-egory <xtheblackeyes@gmail.com>
2021-02-08 23:10:00 +0900

    list data to store

---
+++ store/index.ts
@@ -0,0 +1,55 @@
+import { Module, VuexModule, Mutation } from 'vuex-module-decorators';
+
+interface ItemList {
+    id: Number;
+    content: string;
+    status: string;
+}
+
+@Module({
+    name: 'store',
+    stateFactory: true,
+    namespaced: true,
+})
+export default class Store extends VuexModule {
+    itemList: ItemList[] = [
+        { id: 1, content: '씻기', status: 'clear' },
+        { id: 2, content: '준비하기', status: 'clear' },
+        { id: 3, content: '학교가기', status: 'clear' },
+        { id: 4, content: '게임하기', status: 'yet' },
+    ];
+
+    @Mutation
+    addItem(item: ItemList) {
+        this.itemList.push(item);
+    }
+
+    @Mutation
+    removeItem(id: number) {
+        const idx = this.itemList.findIndex(function(item) {
+            return item.id === id;
+        });
+
+        this.itemList.splice(idx, 1);
+    }
+
+    @Mutation
+    changeStatus(item: ItemList) {
+        const idx = this.itemList.findIndex(function(param) {
+            return param.id === item.id;
+        });
+        if (item.status === 'yet') {
+            this.itemList[idx].status = 'clear';
+        } else {
+            this.itemList[idx].status = 'yet';
+        }
+    }
+
+    get setYet() {
+        return this.itemList.filter((item: ItemList) => item.status === 'yet');
+    }
+
+    get setClear() {
+        return this.itemList.filter((item: ItemList) => item.status === 'clear');
+    }
+}
